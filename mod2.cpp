e29c476ddcfe5ee4b454a3c722085069305d8183
KF <>
2020-04-01 02:15:32 +0900

    fixed pow method and added another static pow method

--- mod2.cpp
+++ mod2.cpp
@@ -2,64 +2,75 @@
 // #define MOD 998244353
 
 class mint {
-public:
-	int i;
+	public:
+		int i;
 
-	mint() : i(0) {}
+		mint() : i(0) {}
 
-	template<class T>
-	mint(T x) {
-		i = int(x % MOD);
-		if (i < 0) i += MOD;
-	}
+		template<class T>
+			mint(T x) {
+				i = int(x % MOD);
+				if (i < 0) i += MOD;
+			}
 
-	mint operator+(mint x) {
-		return i + x.i;
-	}
-
-	mint operator-(mint x) {
-		return i - x.i;
-	}
-
-	mint operator*(mint x) {
-		return (long long)i * x.i;
-	}
-
-	mint operator/(mint x) {
-		return (long long)i * x.pow(MOD - 2).i;
-	}
-
-	template<class T>
-	mint pow(T p) {
-		int r = 1;
-		int t = i;
-		for(; p > 0; p >>= 1) {
-			if (p & 1) r = r * t % MOD;
-			t = t * t % MOD;
+		mint operator+(mint x) {
+			return i + x.i;
 		}
-		return r;
-	}
 
-	mint& operator+=(mint x) {
-		i = (i + x.i) % MOD;
-		return *this;
-	}
+		mint operator-(mint x) {
+			return i - x.i;
+		}
 
-	mint& operator-=(mint x) {
-		i = i - x.i;
-		if (i < 0) i += MOD;
-		return *this;
-	}
+		mint operator*(mint x) {
+			return (long long)i * x.i;
+		}
 
-	mint& operator*=(mint x) {
-		i = (int)((long long)i * x.i % MOD);
-		return *this;
-	}
+		mint operator/(mint x) {
+			return (long long)i * x.pow(MOD - 2).i;
+		}
 
-	mint& operator/=(mint x) {
-		i = (long long)i * x.pow(MOD - 2).i % MOD;
-		return *this;
-	}
+		template<class T>
+			mint pow(T p) {
+				long long r = 1;
+				long long t = i;
+				for(; p > 0; p >>= 1) {
+					if (p & 1) r = r * t % MOD;
+					t = t * t % MOD;
+				}
+				return r;
+			}
+
+		template<class T1, class T2>
+			static mint pow(T1 a, T2 b) {
+				long long r = 1;
+				long long t = (long long)(a % MOD);
+				for(; b > 0; b >>= 1) {
+					if (b & 1) r = r * t % MOD;
+					t = t * t % MOD;
+				}
+				return r;
+			}
+
+		mint& operator+=(mint x) {
+			i = (i + x.i) % MOD;
+			return *this;
+		}
+
+		mint& operator-=(mint x) {
+			i = i - x.i;
+			if (i < 0) i += MOD;
+			return *this;
+		}
+
+		mint& operator*=(mint x) {
+			i = (int)((long long)i * x.i % MOD);
+			return *this;
+		}
+
+		mint& operator/=(mint x) {
+			i = (long long)i * x.pow(MOD - 2).i % MOD;
+			return *this;
+		}
 };
 
 std::ostream& operator<<(std::ostream& os, const mint& m) {
