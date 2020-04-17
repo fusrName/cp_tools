b6abe58197e4be6e57960b618516cc31f1cadd88
KF <>
2020-04-17 21:53:30 +0900

    improved so that comb returns 0 when impossible

--- comb.py
+++ comb.py
@@ -1,12 +1,18 @@
 mod = 10**9 + 7
-facts = [1] * 100001
-for i in range(0, 100000):
-	facts[i+1] = facts[i] * (i + 1) % mod
 
-ifacts = [1] * 100001
-ifacts[100000] = pow(facts[100000], mod - 2, mod)
-for i in range(100000, 0, -1):
-	ifacts[i-1] = ifacts[i] * i % mod
+fact_range = 10**6
+facts = [1] * (fact_range + 1)
+for i in range(0, fact_range):
+    facts[i+1] = facts[i] * (i + 1) % mod
+
+ifacts = [1] * (fact_range + 1)
+ifacts[fact_range] = pow(facts[fact_range], mod - 2, mod)
+for i in range(fact_range, 0, -1):
+    ifacts[i-1] = ifacts[i] * i % mod
 
 def comb(n, k):
-	return facts[n] * ifacts[n-k] % mod * ifacts[k] % mod
+    if k < 0 or n < k:
+        return 0
+    else:
+        return facts[n] * ifacts[n-k] % mod * ifacts[k] % mod
+
