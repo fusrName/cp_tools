99cc8f6a54fbf96bfab6f1d3d79300b1828f687c
KF <>
2020-11-05 05:51:21 +0900

    improved performance of sieve

--- num/sieve.cpp
+++ num/sieve.cpp
@@ -1,26 +1,85 @@
-std::pair<std::vector<int>,std::vector<int>> primes_sieve(int n) {
+std::pair<std::vector<int>,std::vector<int>> primes_sieve(const int n) {
     std::pair<std::vector<int>,std::vector<int>> rv;
     std::vector<int>& primes = rv.first;
     std::vector<int>& sieve = rv.second;
     primes.reserve(n / 10);
     sieve.resize(n + 1, 0);
-    for(int i = 2; i <= n; ++i) {
+    if (n >=  1) sieve[0] = sieve[1] = -1;
+    else if (n >= 0) sieve[0] = -1;
+    if (n <= 4) {
+        for(int i = 2; i <= n; ++i) {
+            if (sieve[i] == 0) {
+                primes.push_back(i);
+                sieve[i] = i;
+            }
+            if (i == 2 && n == 4) sieve[4] = 2;
+        }
+        return rv;
+    }
+    for(int i = 2; i <= n; i += 2) sieve[i] = 2;
+    for(int i = 3; i <= n; i += 6) sieve[i] = 3;
+    primes.push_back(2); primes.push_back(3);
+    const int n3 = n / 3;
+    int i = 5;
+    for(; i <= n3; i += 4) {
         if (sieve[i] == 0) {
             sieve[i] = i;
             primes.push_back(i);
-            for(int p: primes) {
+            int sz = primes.size();
+            for(int j = 2;j < sz; ++j) {
+                int p = primes[j];
                 int x = i * p;
                 if (x > n) break;
                 sieve[x] = p;
             }
         } else {
             int si = sieve[i];
-            for(int p: primes) {
+            for(int j = 2;; ++j) {
+                int p = primes[j];
                 int x = i * p;
                 if (x > n) break;
                 sieve[x] = p;
                 if (p == si) break;
             }
+        }
+        i += 2;
+        if (sieve[i] == 0) {
+            sieve[i] = i;
+            primes.push_back(i);
+            int sz = primes.size();
+            for(int j = 2;j < sz; ++j) {
+                int p = primes[j];
+                int x = i * p;
+                if (x > n) break;
+                sieve[x] = p;
+            }
+        } else {
+            int si = sieve[i];
+            for(int j = 2;; ++j) {
+                int p = primes[j];
+                int x = i * p;
+                if (x > n) break;
+                sieve[x] = p;
+                if (p == si) break;
+            }
+        }
+    }
+    const int n2 = n - 2;
+    for(; i <= n2; i += 4) {
+        if (sieve[i] == 0) {
+            sieve[i] = i;
+            primes.push_back(i);
+        }
+        i += 2;
+        if (sieve[i] == 0) {
+            sieve[i] = i;
+            primes.push_back(i);
+        }
+    }
+    if (i <= n) {
+        if (sieve[i] == 0) {
+            sieve[i] = i;
+            primes.push_back(i);
         }
     }
     return rv;
