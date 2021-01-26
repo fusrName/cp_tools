6e8352767df8c5516187d77400921a0d1837a3cd
KF <>
2021-01-26 23:37:30 +0900

    added another LIS which takes iterators

--- LIS.cpp
+++ LIS.cpp
@@ -21,3 +21,18 @@ int LNDS(std::vector<int>& x) {
     }
     return d.size();
 }
+
+// takes iterators [first, last) as arguments
+template<class T>
+int LIS(T first, T last) {
+    int n = last - first;
+    std::vector<typename std::remove_reference<decltype(*first)>::type> d;
+    d.reserve(n);
+    for(; first != last; first++) {
+        auto xi = *first;
+        auto it = std::lower_bound(d.begin(), d.end(), xi);
+        if (it == d.end()) d.push_back(xi);
+        else *it = xi;
+    }
+    return d.size();
+}
